afd38f4422b9e7eb3a6149c81455bb99e4aa2bb9
Divakar Rayapaty <divakar@Divakars-iMac.local>
2020-08-27 01:51:27 -0400

    Divakar

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b27f063ffceec0bf131d2e46586e33ca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '53be0993-13d2-4ed4-b420-998d9cf57e0e'
+};
